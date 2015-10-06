0ef59276e2b0ab7b6c712bf629d4084dbf15fcf4
Murphy ORourke <murphy.orourke@spr.com>
2015-10-06 13:52:49 -0500

    Simplify code and directory, add new icons sets.

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-##PHP Dynamic Image Directory Viewer
+##Image Directory Viewer
 
-**Modified from [KV Codes Tutorial](http://www.kvcodes.com/2013/12/get-all-images-from-a-directory-dynamically-php-jquery-ajax/)**
-
-- Displays images from a directory in a responsive grid  
-- Shows filename above linked image
+- Displays images from directories in a responsive grid  
+- Displays image filename
+- Click to download image
 - Supports GIF, JPG, PNG, and SVG files
-- Includes Android L SVG icon set, via [shreyasachar](https://github.com/shreyasachar/Android-Resources)  
+- Includes icon sets:
+	- [Android](https://github.com/driftyco/ionicons) (MIT) 
+	- [Entypo](http://www.entypo.com/) (CC BY-SA 4.0)
+	- [Google Emoji](https://github.com/googlei18n/noto-emoji) (Apache V 2.0)
+	- [iOS](https://github.com/driftyco/ionicons) (MIT)
+	- [Social](https://github.com/driftyco/ionicons) (MIT)
+	- [Windows](https://github.com/Templarian/WindowsIcons) (CC BY-ND 3.0)
 
-*Requires running a server*  
+**Scripts Modified from [KV Codes Tutorial](http://www.kvcodes.com/2013/12/get-all-images-from-a-directory-dynamically-php-jquery-ajax/)**
+
+Live server required  
 I &#9829; [MAMP](http://www.mamp.info/en/)  
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- Meta -->
+  <meta name="viewport" content="initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, width=device-width, minimal-ui">
+  <!-- Title -->
+  <title>PHP Dynamic Image Directory Viewer</title>
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <nav>
+      <select>
+        <option value="">Select Icon Set</option>
+        <option value="android">Android</option>
+        <option value="entypo">Entypo</option>
+        <option value="emoji">Google Emoji</option>
+        <option value="ios">iOS</option>
+        <option value="social">Social</option>
+        <option value="windows">Windows</option>
+      </select>
+    </nav>
+
+    <div>
+
+    </div>
+
+  <!-- Javascripts -->
+  <script src="bower_components/jquery/dist/jquery.min.js"></script>
+  <script>
+
+    $('select').change(function() {
+      var selection = $(this).val();
+      $.ajax({
+       url: "scripts/get_" + selection + ".php",
+       type: "POST",
+       dataType: "HTML",
+           success: function( data ) {
+       jQuery('body > div').html(data);
+           },
+           error: function(jqXHR, data ) {
+       alert ('Ajax request Failed.');
+       }
+       });
+    });
+  </script>
+</body>
+</html>
